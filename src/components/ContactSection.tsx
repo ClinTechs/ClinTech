e43420d3035808ef728b707e9ab1c05e8a4fb162
julio <juliocesarcarvalho435@gmail.com>
2025-07-22 16:57:52 -0400

    88888

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -40,7 +40,7 @@ const ContactSection = () => {
     console.log('Dados enviados para o backend:', data);
 
     try {
-      const res = await fetch("https://ctb-theta.vercel.app", {
+      const res = await fetch("http://129.146.125.193:3001/api/SOLICITACOES_ORCAMENTO", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
